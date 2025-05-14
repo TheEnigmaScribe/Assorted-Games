42b386b56d2883df1241aa6bf3a19ff7127e52fe
TheEnigmaScribe <timegearkeeper@gmail.com>
2025-05-14 11:36:33 +0900

    Update to map and enemies
    Setting up stuff- going to later add text file spawning and information for turning and movement paths later on.

--- backstab-(experimental)/Player.gd
+++ backstab-(experimental)/Player.gd
@@ -1,13 +1,28 @@
 extends Sprite2D
 
+signal playerStep
+
 @onready var tile_map = $"../TileMapLayer"
 @onready var sprite_2d: Sprite2D = $PlayerSprite
+@onready var raycast_2d: RayCast2D = $PlayerEnemyCollisionRaycast
 
 var is_moving = false
-var facing = "Down"
+var default_facing = "Down"
+var facing: String
+var raycast_default: Vector2
 
 func _ready():
-	pass
+	if facing == null:
+		facing = default_facing
+	if facing == "Left":
+		raycast_default = Vector2.LEFT
+	elif facing == "Right":
+		raycast_default = Vector2.RIGHT
+	elif facing == "Up":
+		raycast_default = Vector2.UP
+	elif facing == "Down":
+		raycast_default = Vector2.DOWN
+	raycast_2d.target_position = raycast_default * 32
 
 func _physics_process(delta):
 	if is_moving == false:
@@ -23,14 +38,16 @@ func _process(delta):
 		return
 	# if Input.is_action_just_pressed("cDash"):
 	if Input.is_action_just_pressed("stab"):
-		if facing == "Left":
-			pass
-		elif facing == "Right":
-			pass
-		elif facing == "Up":
-			pass
-		elif facing == "Down":
-			pass
+		if raycast_2d.is_colliding():
+			print("functional")
+			if facing == "Left":
+				pass
+			elif facing == "Right":
+				pass
+			elif facing == "Up":
+				pass
+			elif facing == "Down":
+				pass
 		stab(facing)
 	elif Input.is_action_just_pressed("left"):
 		move(Vector2.LEFT)
@@ -54,10 +71,18 @@ func move(direction):
 	
 	var tile_data: TileData = tile_map.get_cell_tile_data(target_tile)
 	
+	# possibly move the "step" variable here, if changing direction but not changing tiles is something I want to count
+	
 	if tile_data.get_custom_data("walkable") == false:
 		return
 	
+	raycast_2d.target_position = direction * 32
+	raycast_2d.force_raycast_update()
+	if raycast_2d.is_colliding():
+		return
+	
 	is_moving = true
+	playerStep.emit()
 	global_position = tile_map.map_to_local(target_tile)
 	sprite_2d.global_position = tile_map.map_to_local(current_tile)
 
--- backstab-(experimental)/Sniper.gd
+++ backstab-(experimental)/Sniper.gd
@@ -1,4 +1,56 @@
 extends Node2D
+	
+@onready var sprite_2d: Sprite2D = $SniperSprite
+@onready var raycast_2d: RayCast2D = $SniperRaycast
 
+# var spinning: bool
+# var moving: bool
+var initialFacing: String
+var facing: String
+# var movementPath: String
+
+func _ready():
+	# set default values here
+# 	spinning = true
+# 	moving = false
+# 	movementPath = "none"
+	facing = "Right"
+	
 func _process(delta):
-	pass
+	if raycast_2d.is_colliding():
+		print("death")
+
+func _on_player_player_step():
+	if facing == "Left":
+		facing = "Up"
+	elif facing == "Up":
+		facing = "Right"
+	elif facing == "Right":
+		facing = "Down"
+	elif facing == "Down":
+		facing = "Left"
+	spriteFacing(facing)
+
+func spriteFacing(looking):
+	var raycast_direction: Vector2
+	if looking == "Left":
+		sprite_2d.texture.region = Rect2(96, 32, 32, 32)
+		raycast_direction = Vector2.LEFT
+	elif looking == "Right":
+		sprite_2d.texture.region = Rect2(64, 0, 32, 32)
+		raycast_direction = Vector2.RIGHT
+	elif looking == "Up":
+		sprite_2d.texture.region = Rect2(64, 32, 32, 32)
+		raycast_direction = Vector2.UP
+	elif looking == "Down":
+		sprite_2d.texture.region = Rect2(96, 0, 32, 32)
+		raycast_direction = Vector2.DOWN
+	
+	# if horizontal view
+	if looking == "Left" or looking == "Right":
+		raycast_2d.target_position = raycast_direction * Vector2(16, 1) * 32
+	
+	# if vertical view
+	if looking == "Up" or looking == "Down":
+		raycast_2d.target_position = raycast_direction * Vector2(1, 16) * 32
+		
