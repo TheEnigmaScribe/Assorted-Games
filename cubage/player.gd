8ffbcc98c6be55177ae54848c5dbd98fd2b37879
TheEnigmaScribe <timegearkeeper@gmail.com>
2025-07-18 21:20:44 +0900

    Cubage Updates
    Basic map and movement now added, but it's incredibly janky. Might be fixable through making all movement directions hold-to-activate, as well as proper gravity and slowing to a stop momentum wise. Or maybe just until you hit a wall? idk

--- cubage/player.gd
+++ cubage/player.gd
@@ -10,71 +10,146 @@ extends CharacterBody2D
 @onready var MagDown: Sprite2D = $DownMagnet
 
 var magnets: Dictionary = {"Left": false, "Right": false, "Up": false, "Down": false}
+var move: Dictionary = {"Left": false, "Right": false, "Up": false, "Down": false}
+var mode = "+"
+var movementDirection: Vector2
+var speed = 500
+var acceleration = 5
 
 func _ready():
 	trigger("Left")
 	trigger("Right")
 	trigger("Up")
 	trigger("Down")
+	trigger("Left")
+	trigger("Right")
+	trigger("Up")
+	trigger("Down")
+
+func _physics_process(delta):
+	collisionCheck()
+	if move["Left"] != "None":
+		if move["Left"] == "Toward":
+			movementDirection.x = -1
+		elif move["Left"] == "Away":
+			movementDirection.x = 1
+	else:
+		movementDirection.x = 0
+	if move["Right"] != "None":
+		if move["Right"] == "Toward":
+			movementDirection.x = 1
+		elif move["Right"] == "Away":
+			movementDirection.x = -1
+	else:
+		movementDirection.x = 0
+	if move["Up"] != "None":
+		if move["Up"] == "Toward":
+			movementDirection.y = -1
+		elif move["Up"] == "Away":
+			movementDirection.y = 1
+	else:
+		movementDirection.y = 0
+	if move["Down"] != "None":
+		if move["Down"] == "Toward":
+			movementDirection.y = 1
+		elif move["Down"] == "Away":
+			movementDirection.y = -1
+	else:
+		movementDirection.y = 0
+	velocity = Vector2(movementDirection.x * speed, movementDirection.y * speed)
+	move_and_slide()
 
 func _process(delta):
-	if Input.is_action_just_pressed("left"):
-		if magnets["Left"]:
-			magnets["Left"] = false
-		elif !magnets["Left"]:
-			magnets["Left"] = true
+	if Input.is_action_just_pressed("shift"):
+		if mode == "+":
+			mode = "-"
+		elif mode == "-":
+			mode = "+"
+	elif Input.is_action_just_pressed("left"):
 		trigger("Left")
 	elif Input.is_action_just_pressed("right"):
-		if magnets["Right"]:
-			magnets["Right"] = false
-		elif !magnets["Right"]:
-			magnets["Right"] = true
 		trigger("Right")
 	elif Input.is_action_just_pressed("up"):
-		print("up")
-		if magnets["Up"]:
-			magnets["Up"] = false
-		elif !magnets["Up"]:
-			magnets["Up"] = true
 		trigger("Up")
 	elif Input.is_action_just_pressed("down"):
-		print("down")
-		if magnets["Down"]:
-			magnets["Down"] = false
-		elif !magnets["Down"]:
-			magnets["Down"] = true
 		trigger("Down")
 
-func trigger(direction):
-	if direction == "Left":
+func collisionCheck():
+	var collider
+	var colliderCharge
+	if RayLeft.is_colliding():
+		collider = RayLeft.get_collider()
+		colliderCharge = collider.get_name().substr(0, 1)
+		if colliderCharge == mode:
+			move["Left"] = "Toward"
+		else:
+			move["Left"] = "Away"
+	else:
+		move["Left"] = "None"
+	
+	if RayRight.is_colliding():
+		collider = RayRight.get_collider()
+		colliderCharge = collider.get_name().substr(0, 1)
+		if colliderCharge == mode:
+			move["Right"] = "Toward"
+		else:
+			move["Right"] = "Away"
+	else:
+		move["Right"] = "None"
+	
+	if RayUp.is_colliding():
+		collider = RayUp.get_collider()
+		colliderCharge = collider.get_name().substr(0, 1)
+		if colliderCharge == mode:
+			move["Up"] = "Toward"
+		else:
+			move["Up"] = "Away"
+	else:
+		move["Up"] = "None"
+	if RayDown.is_colliding():
+		collider = RayDown.get_collider()
+		colliderCharge = collider.get_name().substr(0, 1)
+		if colliderCharge == mode:
+			move["Down"] = "Toward"
+		else:
+			move["Down"] = "Away"
+	else:
+		move["Down"] = "None"
+
+func trigger(dir):
+	if dir == "Left":
 		if magnets["Left"]:
-			MagLeft.show()
-			RayLeft.enabled = true
-		else: 
+			magnets["Left"] = false
 			MagLeft.hide()
 			RayLeft.enabled = false
-		return
-	elif direction == "Right":
+		elif !magnets["Left"]:
+			magnets["Left"] = true
+			MagLeft.show()
+			RayLeft.enabled = true
+	elif dir == "Right":
 		if magnets["Right"]:
-			MagRight.show()
-			RayRight.enabled = true
-		else:
+			magnets["Right"] = false
 			MagRight.hide()
 			RayRight.enabled = false
-		return
-	elif direction == "Up":
+		elif !magnets["Right"]:
+			magnets["Right"] = true
+			MagRight.show()
+			RayRight.enabled = true
+	elif dir == "Up":
 		if magnets["Up"]:
-			MagUp.show()
-			RayUp.enabled = true
-		else:
+			magnets["Up"] = false
 			MagUp.hide()
 			RayUp.enabled = false
-		return
-	elif direction == "Down":
+		elif !magnets["Up"]:
+			magnets["Up"] = true
+			MagUp.show()
+			RayUp.enabled = true
+	elif dir == "Down":
 		if magnets["Down"]:
-			MagDown.show()
-			RayDown.enabled = true
-		else:
+			magnets["Down"] = false
 			MagDown.hide()
 			RayDown.enabled = false
-		return
+		elif !magnets["Down"]:
+			magnets["Down"] = true
+			MagDown.show()
+			RayDown.enabled = true
